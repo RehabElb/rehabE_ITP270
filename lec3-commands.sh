88519045f4a892d6b7676baaed7b369e8550f6d9
Rehab E <rehabelbermawy19@gmail.com>
2022-01-25 21:39:16 -0500

    Adding lecture3 file with folder structure

---
+++ lec3-commands.sh
@@ -0,0 +1,86 @@
+
+   73  ls
+   74  cd rehabE_ITP270
+   75  ls
+   76  nano lecture3.txt
+   77  cat lecture3.txt
+   78  ls
+   79  nano lecture3.txt
+   80  cat lecture3.txt
+   81  git status
+   82  git add -A
+   83  git status
+   84  git commit -m "Adding lecture3 file with folder structure"
+   85  git push
+   86  cd ~
+   87  pwd
+   88  mkdir Movies
+   89  cd Movies
+   90  mkdir Action
+   91  mkdir Comedy
+   92  mkdir Drama
+   93  ls
+   94  touch genres.txt
+   95  ls
+   96  cd Action
+   97  mkdir scifi
+   98  cd scifi
+   99  touch terminator.txt
+  100  ls
+  101  cd ..
+  102  mkdir superhero
+  103  touch batman.txt
+  104  touch matrix-reloaded.txt
+  105  touch matrix-revolutions.txt
+  106  touch matrix.txt
+  107  touch superman.txt
+  108  touch wonderwoman.txt
+  109  cd ..
+  110  cd Comedy
+  111  mkdir slapstick
+  112  mkdir satire
+  113  touch the-office.txt
+  114  ls
+  115  cd slapstick
+  116  touch waterboy.txt
+  117  touch zoolander.txt
+  118  ls
+  119  cd ..
+  120  cd satire
+  121  touch figtclub.txt
+  122  cd ..
+  123  ls
+  124  cd Drama
+  125  mkdir biopic
+  126  ls
+  127  cd biopic
+  128  touch cleopatra.txt
+  129  touch licoln.txt
+  130  touch frida.txt
+  131  touch notorious.txt
+  132  touch ray.txt
+  133  ls
+  134  cd ..
+  135  mkdir historical
+  136  ls
+  137  cd historical
+  138  touch gladitor.txt
+  139  touch selma.txt
+  140  ls
+  141  cd ..
+  142  ls
+  143  cd ..
+  144  ls
+  145  cd 
+  146  cd Movies
+  147  ls
+  148  touch file1.txt file2.txt file3.txt
+  149  ls
+  150  rm file1.txt
+  151  ls
+  152  rm file2.txt file3.txt
+  153  ls
+  154  cd ..
+  155  tree
+  156  history
+  157  history > /home/kali/rehabE_ITP270/lec3-commands.txt
